10f1a2d364731ff7ec2c94c2f3aecfa67a987f16
syah <syahfisal37@yahoo.com>
2017-05-12 17:51:29 +0800

    'ignorenode_modules'

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-var app=express();
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
 
+var google=require('googleapis');
 
+var gmail=google.gmail('v1');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extend: true}));
-app.use(bodyParser.text({type: '*/*'}));
 
+var port = process.env.PORT || 8080;        // set our port
 
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-var route=require('./routes/route.js')(app);
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
 
-var server= app.listen(process.env.port || 3000,function(){
-	console.log("listening on port %s",server.address().port);
-})
+router.post('/',function(req,res){
+		var obj=req.body
+		var auth=req.body.auth;
+		res.json(req.body);
+		// gmail.users.messages.list({
+		//     auth:auth,
+		//     userId:'me',
+		//     labelIds:['CATEGORY_PERSONAL'],
+		//     maxResults:100,
+		//   },function(err,response){
+
+		//   });
+		
+	});
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+//Start the server 
+app.listen(port);
+console.log('Magic happens on port ' + port);
