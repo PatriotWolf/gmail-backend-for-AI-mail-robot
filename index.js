fd4c2d9b6167bf6088dbe974e7e500b8c6b7dcf5
syah <syahfisal37@yahoo.com>
2017-05-12 16:47:06 +0800

    'mailgrabber'

---
+++ index.js
@@ -0,0 +1,16 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var app=express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(bodyParser.text({type: '*/*'}));
+
+
+
+var route=require('./routes/route.js')(app);
+
+var server= app.listen(3000,function(){
+	console.log("listening on port %s",server.address().port);
+})
---
+++ routes/route.js
@@ -0,0 +1,24 @@
+
+var google=require('googleapis');
+
+var gmail=google.gmail('v1');
+
+var appRouter= function (app)
+{	app.get("/",function(req,res){
+		res.send("Hello World");
+	});
+	app.post("/",function(req,res){
+		var obj=req.body
+		var auth=req.body.auth;
+		gmail.users.messages.list({
+		    auth:auth,
+		    userId:'me',
+		    labelIds:['CATEGORY_PERSONAL'],
+		    maxResults:100,
+		  },function(err,response){
+
+		  });
+		res.send(JSON.stringify(auth));
+	});
+}
+module.exports=appRouter;
