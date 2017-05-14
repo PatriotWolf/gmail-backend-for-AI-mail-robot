1fba5a402da9b84367bf5d7981c83efa211afb0b
syah <syahfisal37@yahoo.com>
2017-05-14 20:19:05 +0800

    'ignorenode_modules'

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var google=require('googleapis');
 var googleAuth = require('google-auth-library');
+var verifier = require('google-id-token-verifier');
 var request = require('request');
 
 var gmail=google.gmail('v1');
@@ -29,24 +30,12 @@ router.post('/',function(req,res){
 		var id=req.body.clientID;
 		
 		//this is where to decode or know what to do with the token/id_token/ anything?
-		var options = {
-		  method: 'POST',
-		  url: 'www.googleapis.com/oauth2/v4/token',
-		  headers: {
-		    'content-type': 'x-www-form-urlencoded'
-		  },
-			code:token,
-	client_id:'916867267412-61vafvip23245d2hqmqujjvmbm5vjklq.apps.googleusercontent.com',
-	client_secret:'C4Bmg6RpT9KWk-Srkk6miSfp',
-	redirect_uri:'https://airobotapi.herokuapp.com/api/',
-	grant_type: 'authorization_code'
-
-
-		};
-		request(options, function(err,response,body){
-				
-						res.json(body);
-
+		var clientId = '916867267412-61vafvip23245d2hqmqujjvmbm5vjklq.apps.googleusercontent.com';
+				verifier.verify(token, clientId, function (err, tokenInfo) {
+		  if (!err) {
+		    // use tokenInfo in here. 
+		    console.log(tokenInfo);
+		  }
 		});
 
 	});
