b9e9b871b355e37b2bb9057149e5c8a3665e74c3
syah <syahfisal37@yahoo.com>
2017-05-14 22:06:38 +0800

    'ignorenode_modules'

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+
 var google=require('googleapis');
 var googleAuth = require('google-auth-library');
 var verifier = require('google-id-token-verifier');
 var request = require('request');
 
 var gmail=google.gmail('v1');
+var credentials;
+
+//read app credentials
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -28,13 +33,20 @@ router.post('/',function(req,res){
 		//var obj=req.body
 		var token=req.body.auth;
 		var id=req.body.clientID;
-		
+		console.log(token);
 		//this is where to decode or know what to do with the token/id_token/ anything?
 		var clientId = '916867267412-61vafvip23245d2hqmqujjvmbm5vjklq.apps.googleusercontent.com';
 				verifier.verify(token, clientId, function (err, tokenInfo) {
 		  if (!err) {
 		    // use tokenInfo in here. 
-		    res.json(tokenInfo);
+		    //res.json(tokenInfo);
+		    	var Gmail = require('node-gmail-api')
+				  , gmail = new Gmail(token)
+				  , s = gmail.messages('label:inbox', {max: 10})
+				 
+				s.on('data', function (d) {
+				  res.json(d);
+				})
 		  }
 		  if(err)
 		  {
