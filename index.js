fec7e03bd78ac61d7b51aadf58831dbdf92e0e44
syah <syahfisal37@yahoo.com>
2017-05-14 18:45:32 +0800

    'ignorenode_modules'

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+var fs = require('fs');
 var google=require('googleapis');
 var googleAuth = require('google-auth-library');
-
+var googlecontent;
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+		googlecontent=content;
+		console.log("read the client secret")
+});
+var auth = new googleAuth();
+var oauth2Client=new auth.OAuth2(clientId, clientSecret, redirectUrl);
 var gmail=google.gmail('v1');
 
 // configure app to use bodyParser()
@@ -27,21 +33,17 @@ router.post('/',function(req,res){
 		var obj=req.body
 		var auth=req.body.auth;
 		var id=req.body.clientID;
-		var auth = new googleAuth;
-var client = new auth.OAuth2(id,'','')
-		client.verifyIdToken(
-		    auth,
-		    id,
-		    // Or, if multiple clients access the backend:
-		    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
-		    function(e, login) {
-		      var payload = login.getPayload();
-		      var userid = payload['sub'];
-		      var obj={"payload":payload,"userid":userid}
-		      res.json(obj);
-		      // If request specified a G Suite domain:
-		      //var domain = payload['hd'];
-	    });
+		var clientSecret = credentials.installed.client_secret;
+	 	var clientId = credentials.installed.client_id;
+	  	var redirectUrl = credentials.installed.redirect_uris[0];
+	  	 var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+		oauth2Client.getToken(req.body.auth, function (err, tokens) {
+		  res.json(tokens)
+		  // Now tokens contains an access_token and an optional refresh_token. Save them.
+		  if (!err) {
+		    oauth2Client.setCredentials(tokens);
+		  }
+		});
 	});
 // more routes for our API will happen here
 
