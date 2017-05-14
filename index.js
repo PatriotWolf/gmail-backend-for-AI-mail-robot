cf93c3ac879db348e38338781a60ab6ae68fd234
syah <syahfisal37@yahoo.com>
2017-05-14 20:06:39 +0800

    'ignorenode_modules'

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var google=require('googleapis');
 var googleAuth = require('google-auth-library');
-var googlecontent;
-fs.readFile('client_secret.json','utf8', function processClientSecrets(err, content) {
-	if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-		googlecontent=JSON.parse(content);
-		console.log("read the client secret");
-		console.log(googlecontent.web.client_secret);
-});
+var request = require('request');
 
 var gmail=google.gmail('v1');
 
@@ -34,21 +24,29 @@ router.get('/', function(req, res) {
 });
 
 router.post('/',function(req,res){
-		var obj=req.body
-		var auth=req.body.auth;
+		//var obj=req.body
+		var token=req.body.auth;
 		var id=req.body.clientID;
-		var clientSecret = googlecontent.web.client_secret;
-	 	var clientId = googlecontent.web.client_id;
-	  	var redirectUrl = googlecontent.web.redirect_uris[0];
-	  	var auth = new googleAuth();
-	  	 var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-		oauth2Client.getToken(req.body.auth, function (err, tokens) {
-		  res.json(tokens)
-		  // Now tokens contains an access_token and an optional refresh_token. Save them.
-		  if (!err) {
-		    oauth2Client.setCredentials(tokens);
-		  }
+		
+		//this is where to decode or know what to do with the token/id_token/ anything?
+		var options = {
+		  method: 'POST',
+		  url: 'www.googleapis.com/oauth2/v4/token',
+		  headers: {
+		    'content'
+		  },
+			code:token,
+	client_id:'916867267412-61vafvip23245d2hqmqujjvmbm5vjklq.apps.googleusercontent.com',
+	client_secret:'C4Bmg6RpT9KWk-Srkk6miSfp',
+	redirect_uri:'https://airobotapi.herokuapp.com/api/',
+	grant_type: 'authorization_code'
+
+
+		};
+		request(options, function(err,response,body){
+				res.json(body);
 		});
+
 	});
 // more routes for our API will happen here
 
