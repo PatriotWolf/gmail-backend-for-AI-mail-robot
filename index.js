7a755453f60e4344daf3f67ab9a30dfff9addbb5
syah <syahfisal37@yahoo.com>
2017-05-15 23:26:15 +0800

    'ahaha'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 var google=require('googleapis');
-
+var OAuth2 = google.auth.OAuth2;
 
 
 var googleAuth = require('google-auth-library');
@@ -45,32 +45,23 @@ router.post('/',function(req,res){
 		var id=req.body.clientID;
 		console.log(token);
 		//use the access code
-				
-				oauth2Client.getToken(token, function (err, tokens) {
-				  // Now tokens contains an access_token and an optional refresh_token. Save them.
-				  if (!err) {
-				    oauth2Client.setCredentials(tokens);
-					console.log(oauth2Client);
-					gmail.users.messages.list({
-					    auth:oauth2Client,
-					    userId:id,
-					    labelIds:['CATEGORY_PERSONAL'],
-					    maxResults:10,
-					  },function(err,response){
-					  		if(err)
-					  	{		res.json("error :" + err);
+				var cred={"access_token":token};
+			oauth2Client.setCredentials(cred);
+			console.log(oauth2Client);
+		gmail.users.messages.list({
+		    auth:oauth2Client,
+		    userId:'me',
+		    labelIds:['CATEGORY_PERSONAL'],
+		    maxResults:10,
+		  },function(err,response){
+		  		if(err)
+		  	{		res.json("error :" + err);
 
 
-					  	}
-					  	else{res.json(response);}
+		  	}
+		  	else{res.json(response);}
 
-					  });
-				  }
-				  else{
-				  	res.json("Back end :"+err)
-				  }
-				});
-			
+		  });
 
 
 	});
